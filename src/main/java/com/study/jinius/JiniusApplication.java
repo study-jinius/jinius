6d29d06567c8c650aee4b12d519e52c9b71d3fbc
jduckling_1024 <jduck1024@naver.com>
2022-09-13 21:46:18 +0900

    JIN-1 프로젝트 세팅

---
+++ src/main/java/com/study/jinius/JiniusApplication.java
@@ -0,0 +1,13 @@
+package com.study.jinius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiniusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiniusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/jinius/JiniusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.jinius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiniusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
