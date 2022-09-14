83b273c699f218e2e3f8c7de88b263dac5319ddd
jduckling_1024 <jduck1024@naver.com>
2022-09-14 22:17:13 +0900

    JIN-4 PostController에 Swagger 요약 추가

--- src/main/java/com/study/jinius/post/controller/PostController.java
+++ src/main/java/com/study/jinius/post/controller/PostController.java
@@ -3,6 +3,7 @@ package com.study.jinius.post.controller;
 import com.study.jinius.common.model.CommonResponse;
 import com.study.jinius.post.model.*;
 import com.study.jinius.post.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -10,29 +11,33 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/post")
+@RequestMapping("/api/post")
 @RequiredArgsConstructor
 public class PostController {
     private final PostService postService;
 
+    @Operation(summary = "게시물 작성", tags = "PostController")
     @PostMapping
     public CommonResponse<CreatePostResponse> createPost(@RequestBody CreatePostRequest request) {
         CreatePostResponse response = postService.createPost(request.toParam());
         return new CommonResponse<>(HttpStatus.OK, response);
     }
 
+    @Operation(summary = "게시물 수정", tags = "PostController")
     @PutMapping
     public CommonResponse<UpdatePostResponse> updatePost(@RequestBody UpdatePostRequest request) {
         UpdatePostResponse response = postService.updatePost(request.toParam());
         return new CommonResponse<>(HttpStatus.OK, response);
     }
 
+    @Operation(summary = "게시물 삭제", tags = "PostController")
     @DeleteMapping("/{idx}")
     public CommonResponse<Long> deletePost(@PathVariable Long idx) {
         Long deletedPostIdx = postService.deletePost(idx);
         return new CommonResponse<>(HttpStatus.OK, deletedPostIdx);
     }
 
+    @Operation(summary = "게시물 리스트 조회", tags = "PostController")
     @GetMapping("/list")
     public CommonResponse<List<PostResponse>> getList() {
         List<PostResponse> responseList = postService.getList();
