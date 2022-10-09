3f5edbcc362bf78012fc30437c2ef5124ac0bae4
jduckling_1024 <jduck1024@naver.com>
2022-10-09 19:48:20 +0900

    JIN-11 JPA Auditing 기능 활성화

--- src/main/java/com/study/jinius/JiniusApplication.java
+++ src/main/java/com/study/jinius/JiniusApplication.java
@@ -2,7 +2,11 @@ package com.study.jinius;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+// @EnableJpaAuditing : JPA 감시 기능 활성화.
+// DB에 데이터가 저장되거나 수정될 때 언제 누가 했는지를 자동으로 관리할 수 있게 한다.
+@EnableJpaAuditing
 @SpringBootApplication
 public class JiniusApplication {
 
