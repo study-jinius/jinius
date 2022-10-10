df460ba2b4855d2672d7233870927c5656507862
jduckling_1024 <jduck1024@naver.com>
2022-10-10 21:48:05 +0900

    JIN-16 Swagger에서 테스트할 각 기능에 Access Token 포함할 수 있도록 설정하기

--- src/main/java/com/study/jinius/common/config/SwaggerConfig.java
+++ src/main/java/com/study/jinius/common/config/SwaggerConfig.java
@@ -2,8 +2,11 @@ package com.study.jinius.common.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,11 +20,23 @@ public class SwaggerConfig {
 
     @Bean
     public GroupedOpenApi groupedOpenApi() {
-        String[] paths = {"/posts"};
+        String[] paths = {"/**"};
 
         return GroupedOpenApi.builder()
                 .group("jinius")
                 .pathsToMatch(paths)
+                .addOpenApiCustomiser(buildSecurityOpenApi())
                 .build();
+    }
+
+    public OpenApiCustomiser buildSecurityOpenApi() {
+        return OpenApi -> OpenApi.addSecurityItem(new SecurityRequirement().addList("jwt token"))
+                .getComponents()
+                .addSecuritySchemes("jwt token", new SecurityScheme()
+                        .name("Authorization")
+                        .type(SecurityScheme.Type.HTTP)
+                        .in(SecurityScheme.In.HEADER)
+                        .bearerFormat("JWT")
+                        .scheme("Bearer"));
     }
 }
